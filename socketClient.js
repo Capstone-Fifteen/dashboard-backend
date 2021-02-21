a5d459773c25f2160173f68c2e1401180881be13
xhxh96 <xianhao@u.nus.edu>
2021-02-21 20:00:26 +0800

    updated client script to simulate streaming data

--- socketClient.js
+++ socketClient.js
@@ -1,15 +1,52 @@
 // Node.js socket client script
 const net = require('net');
-const client = net.createConnection({ port: 3000 }, () => {
+
+const generateRandomFloat = (min, max) => {
+  const i = Math.random() * (max - min) + min
+
+  // Truncate to 2 dp
+  return i.toFixed(2)
+}
+
+const generateRandomNumber = (min, max) => {
+  const i = Math.random() * (max - min) + min
+
+  return Math.floor(i)
+}
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+const writeRawData = (client) => {
+  const x_reading = generateRandomFloat(0, 100)
+  const y_reading = generateRandomFloat(0, 100)
+  const z_reading = generateRandomFloat(0, 100)
+  const roll = generateRandomFloat(0, 100)
+  const pitch = generateRandomFloat(0, 100)
+  const yaw = generateRandomFloat(0, 100)
+  const emg_reading = generateRandomNumber(0, 100)
+  const currentTIme = new Date().toISOString()
+
+  const message = `@${x_reading}, ${y_reading}, ${z_reading}|${roll}, ${pitch}, ${yaw}|${emg_reading}|1|${currentTIme}`
+  console.log(message)
+
+  client.write(message)
+}
+
+const client = net.createConnection({ port: 3000, host: '***REMOVED***' }, async () => {
   console.log('Connected to server.');
-  const currentTime = new Date().toISOString()
-  // client.write(`#3|dab|3|1|${currentTime}`);
-  client.write(`@1.24, 2.13, 1.44|54.3, 21.0, 44.8|56|1|${currentTime}`);
+
+  // Send 20 raw data points at 1 second apart
+  for (let i = 0; i < 10; i++) {
+    // client.write(`#3|dab|3|1|${currentTime}`);
+    // client.write(`@8.21, 10.24, 6.05|10.25, 33.8, 5.85|6|1|${currentTime}`);
+    writeRawData(client)
+    await timer(1000)
+  }
+  client.end()
 });
-client.on('data', (data) => {
-  console.log(data.toString());
-  client.end();
-});
+client.on('error', () => {
+  console.log('Error connecting to server.')
+})
 client.on('end', () => {
   console.log('Disconnected from server.');
 })
